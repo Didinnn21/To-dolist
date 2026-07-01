0faef3d6d38a0bba25ad7a43b6f54bfe99a70ac5
Notkizzy <kizzy.net2@gmail.com>
2026-07-02 00:07:23 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -7,6 +7,7 @@ if (isset($_SERVER['VERCEL_URL']) || isset($_ENV['VERCEL_URL'])) {
         '/tmp/storage/framework/sessions',
         '/tmp/storage/logs',
         '/tmp/storage/app',
+        '/tmp/storage/bootstrap/cache',
     ];
     foreach ($dirs as $dir) {
         if (!is_dir($dir)) {
@@ -18,6 +19,11 @@ if (isset($_SERVER['VERCEL_URL']) || isset($_ENV['VERCEL_URL'])) {
     putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
     putenv('SESSION_DRIVER=cookie'); // Hindari file session di serverless
     putenv('LOG_CHANNEL=stderr');
+    putenv('APP_SERVICES_CACHE=/tmp/storage/bootstrap/cache/services.php');
+    putenv('APP_PACKAGES_CACHE=/tmp/storage/bootstrap/cache/packages.php');
+    putenv('APP_CONFIG_CACHE=/tmp/storage/bootstrap/cache/config.php');
+    putenv('APP_ROUTES_CACHE=/tmp/storage/bootstrap/cache/routes.php');
+    putenv('APP_EVENTS_CACHE=/tmp/storage/bootstrap/cache/events.php');
 }
 
 // Meneruskan request dari Vercel ke index.php bawaan Laravel
